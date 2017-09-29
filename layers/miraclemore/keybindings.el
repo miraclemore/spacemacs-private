b8b4fae9caf775a8ddc2384595da013f0e0277df
miracle.huang <miraclemore.huang@gmail.com>
2017-09-29 16:35:41 +0800

    Add keybindings.el for miraclemore layer

---
+++ layers/miraclemore/keybindings.el
@@ -0,0 +1,15 @@
+;;; keybindings.el --- miraclemore Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2016-2017 Miracle Huang
+;;
+;; Author: miracle.huang <miraclemore.huang@gmail.com>
+;; URL:
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(define-key 'help-command (kbd "C-f") 'find-function)
+(define-key 'help-command (kbd "C-k") 'find-function-on-key)
+(define-key 'help-command (kbd "C-v") 'find-variable)
+(define-key 'help-command (kbd "C-l") 'find-library)
