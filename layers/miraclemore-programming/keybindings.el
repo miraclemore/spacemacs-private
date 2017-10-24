e3ded07c49227f262f56870ba399eb3660eb35f2
miracle.huang <miraclemore.huang@gmail.com>
2017-10-24 16:45:17 +0800

    set key M-. to call helm-gtags-dwim

---
+++ layers/miraclemore-programming/keybindings.el
@@ -0,0 +1,12 @@
+;;; keybindings.el --- miraclemore-programming Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2017 Miracle Huang
+;;
+;; Author: miracle.huang <miraclemore.huang@gmail.com>
+;; URL:
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(define-key evil-normal-state-map (kbd "M-.") 'helm-gtags-dwim)
